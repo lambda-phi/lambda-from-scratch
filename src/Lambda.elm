40ee3967decd7abe983a361fd0f93112e5859f25
David Cavazos <dcavazos@google.com>
2021-07-05 17:39:47 -0700

    add some rule comments

--- src/Lambda.elm
+++ src/Lambda.elm
@@ -1,6 +1,6 @@
 module Lambda exposing
     ( Error(..)
-    , Term(..)
+    , Expr(..)
     , eval
     )
 
@@ -20,9 +20,27 @@ import Parser
 
 {-| A Lambda calculus Expression.
 
+Closure calculus:
 <https://www.youtube.com/watch?v=ogXlQf8lDD4>
 <https://slides.yowconference.com/yowlambdajam2019/XuanyiChew-AnAlienLambdaCalculus.pdf>
 <https://blog.chewxy.com/wp-content/uploads/personal/dissertation31482.pdf>
+
+Bidirectional Type Checking
+<https://youtu.be/utyBNDj7s2w>
+
+An Introduction to Polymorphic Lambda Calculus with Subtyping
+<http://soft.vub.ac.be/Publications/1994/vub-tinf-tr-94-01.pdf>
+
+Complete and Easy Bidirectional Typechecking for Higher-Rank Polymorphism
+<https://www.cl.cam.ac.uk/~nk480/bidir.pdf>
+
+-- map : (a -> b) -> Vec n a -> Vec n b
+-- toStr : Int -> Str
+-- xs : Vec 7 Int
+
+-- map toStr xs -- map : (Int -> Str) -> Vec 7 Int -> Vec 7 Str
+
+-- Dependent types examples: map and filter with Vec
 
 -- type Maybe a = Just a | Nothing
 -- Maybe : = λa. (a -> b) -> b -> b
@@ -30,28 +48,31 @@ import Parser
 -- Nothing : Maybe a = λj n. n
 
 -}
-type Term
+type Expr
     = Type --               Type        Type of types (Kind)
     | IntT --               Int         Integer type
     | NumT --               Num         Number type
     | Int Int --            42          Integer value
     | Num Float --          3.14        Number value
     | Var String --         x           Variable
-    | Fun Term Term --      T1 -> T2    Function type
-    | Lam String Term --    λx. t       Lambda abstraction
-    | App Term Term --      t1 t2       Application
+    | Fun Expr Expr --      t1 -> t2    Function type
+    | Lam String Expr --    λx. e       Lambda abstraction
+    | App Expr Expr --      e1 e2       Application
+    | Ann Expr Expr --      e : t       Type annotation
 
 
 {-| An error from parsing or evaluation.
 -}
 type Error
     = SyntaxError Parser.Error
-    | CannotApply Term Term
+    | VariableNotFound String
+    | CannotApply Expr Expr
+    | TypeMismatch Expr Expr
 
 
 {-|
 
-    import Lambda exposing (Error(..), Term(..), eval)
+    import Lambda exposing (Error(..), Expr(..), eval)
 
     -- Types
     eval Type --> Ok ( Type, Type )
@@ -63,18 +84,19 @@ type Error
     eval (Num 3.14) --> Ok ( Num 3.14, NumT )
 
     -- Variable
-    eval (Var "x") --> Ok ( Var "x", Var "x" )
+    eval (Var "x") --> Err (VariableNotFound "x")
 
     -- Function type
-    eval (Fun (Var "a") IntT) --> Ok ( Fun (Var "a") IntT, Fun (Var "a") Type )
+    eval (Fun IntT Type) --> Ok ( Fun IntT Type, Fun Type Type )
+    -- eval (Fun (Var "a") IntT) -- Ok ( Fun (Var "a") IntT, Fun (Var "a") Type )
 
     -- Lambda abstraction
     eval (Lam "x" Type)                            --> Ok ( Lam "x" Type, Fun (Var "x") Type )
     eval (Lam "x" IntT)                            --> Ok ( Lam "x" IntT, Fun (Var "x") Type )
     eval (Lam "x" (Int 1))                         --> Ok ( Lam "x" (Int 1), Fun (Var "x") IntT )
-    eval (Lam "x" (Var "x"))                       --> Ok ( Lam "x" (Var "x"), Fun (Var "x") (Var "x") )
-    eval (Lam "x" (Fun (Var "x") IntT))            --> Ok ( Lam "x" (Fun (Var "x") IntT), Fun (Var "x") (Fun (Var "x") Type) )
-    eval (Lam "x" (Lam "y" (Var "x")))             --> Ok ( Lam "x" (Lam "y" (Var "x")), Fun (Var "x") (Fun (Var "y") (Var "x")) )
+    -- eval (Lam "x" (Var "x"))                       -- Ok ( Lam "x" (Var "x"), Fun (Var "x") (Var "x") )
+    -- eval (Lam "x" (Fun (Var "x") IntT))            -- Ok ( Lam "x" (Fun (Var "x") IntT), Fun (Var "x") (Fun (Var "x") Type) )
+    -- eval (Lam "x" (Lam "y" (Var "x")))             -- Ok ( Lam "x" (Lam "y" (Var "x")), Fun (Var "x") (Fun (Var "y") (Var "x")) )
     eval (Lam "x" (App (Lam "y" (Int 1)) (Int 2))) --> Ok ( Lam "x" (Int 1), Fun (Var "x") IntT )
 
     -- Application
@@ -83,20 +105,31 @@ type Error
     eval (App (Int 0) (Int 1))                                     --> Err (CannotApply (Int 0) (Int 1))
     eval (App (Var "x") (Int 1))                                   --> Err (CannotApply (Var "x") (Int 1))
     eval (App (Fun IntT Type) (Int 1))                             --> Err (CannotApply (Fun IntT Type) (Int 1))
+    eval (App (App (Lam "x" (Lam "y" (Var "x"))) (Int 1)) (Int 2)) --> Ok ( Int 1, IntT )
+
+    -- Let closure
     eval (App (Lam "x" Type) (Int 1))                              --> Ok ( Type, Type )
     eval (App (Lam "x" IntT) (Int 1))                              --> Ok ( IntT, Type )
     eval (App (Lam "x" (Int 0)) (Int 1))                           --> Ok ( Int 0, IntT )
     eval (App (Lam "x" (Var "x")) (Int 1))                         --> Ok ( Int 1, IntT )
-    eval (App (Lam "x" (Var "y")) (Int 1))                         --> Ok ( Var "y", Var "y" )
+    -- eval (App (Lam "x" (Var "y")) (Int 1))                         -- Ok ( Var "y", Var "y" )
     eval (App (Lam "x" (Fun (Var "x") Type)) (Int 1))              --> Ok ( Fun (Int 1) Type, Fun IntT Type )
     eval (App (Lam "x" (Lam "y" (Var "x"))) (Int 1))               --> Ok ( Lam "y" (Int 1), Fun (Var "y") IntT )
     eval (App (Lam "x" (App (Lam "y" (Int 0)) Type)) (Int 1))      --> Ok ( Int 0, IntT )
-    eval (App (App (Lam "x" (Lam "y" (Var "x"))) (Int 1)) (Int 2)) --> Ok ( Int 1, IntT )
+
+    -- Type annotation
+    eval (Ann Type Type) --> Ok ( Type, Type )
+    eval (Ann IntT Type) --> Ok ( IntT, Type )
+    eval (Ann Type IntT) --> Err (TypeMismatch Type IntT)
+    eval (Ann (Int 1) IntT) --> Ok ( Int 1, IntT )
+    eval (Ann (Int 1) (Int 2)) --> Err (TypeMismatch IntT (Int 2))
+    -- TODO: Ann Var *
+    eval (Ann )
 
 -}
-eval : Term -> Result Error ( Term, Term )
-eval term =
-    case term of
+eval : Expr -> Result Error ( Expr, Expr )
+eval expr =
+    case expr of
         Type ->
             Ok ( Type, Type )
 
@@ -113,41 +146,69 @@ eval term =
             Ok ( Num n, NumT )
 
         Var x ->
-            Ok ( Var x, Var x )
+            Err (VariableNotFound x)
 
-        Fun term1 term2 ->
+        Fun e1 e2 ->
             Result.map2
                 (\( t1, k1 ) ( t2, k2 ) -> ( Fun t1 t2, Fun k1 k2 ))
-                (eval term1)
-                (eval term2)
+                (eval e1)
+                (eval e2)
 
-        Lam x body ->
+        Lam x e ->
             Result.map
-                (\( t, k ) -> ( Lam x t, Fun (Var x) k ))
-                (eval (App (Lam x body) (Var x)))
+                (\( ee, k ) -> ( Lam x ee, Fun (Var x) k ))
+                (eval (App (Lam x e) (Var x)))
 
-        App (Lam x (Fun term1 term2)) value ->
-            eval (Fun (App (Lam x term1) value) (App (Lam x term2) value))
+        -- x=ex; e1 -> e2
+        -- ⇒  (x=ex; e1) (x=ex; e2)
+        App (Lam x (Fun e1 e2)) ex ->
+            eval (Fun (App (Lam x e1) ex) (App (Lam x e2) ex))
 
-        App (Lam x (Lam y body)) value ->
-            eval (Lam y (App (Lam x body) value))
+        -- x=ex; λy. e
+        -- ⇒  λy. (x=ex; e)
+        -- ⇒  λy. ((λx. e) ex)
+        App (Lam x (Lam y e)) ex ->
+            eval (Lam y (App (Lam x e) ex))
 
-        App (Lam x (App (Lam y body) valueY)) valueX ->
+        -- x=ex; y=ey; e
+        -- ⇒  y=(x=ex; ey); (x=ex; e)
+        -- ⇒  y=((λx. ey) ex); ((λx. e) ex)
+        -- ⇒  (λy. ((λx. e) ex)) ((λx. ey) ex)
+        App (Lam x (App (Lam y e) ey)) ex ->
             Result.andThen
-                (\( valY, _ ) -> eval (App (Lam y body) valY))
-                (eval (App (Lam x valueY) valueX))
+                (\( vy, _ ) -> eval (App (Lam y e) vy))
+                (eval (App (Lam x ey) ex))
 
-        App (Lam x body) value ->
-            if body == Var x then
-                eval value
+        App (Lam x e) ex ->
+            if e == Var x then
+                -- x=ex; x  ⇒  ex
+                eval ex
 
             else
-                eval body
+                -- x=ex; e  ⇒  e
+                eval e
 
-        App ((App _ _) as app) t2 ->
+        App ((App _ _) as app) e2 ->
             Result.andThen
-                (\( t1, _ ) -> eval (App t1 t2))
+                (\( e1, _ ) -> eval (App e1 e2))
                 (eval app)
 
-        App t1 t2 ->
-            Err (CannotApply t1 t2)
+        App e1 e2 ->
+            Err (CannotApply e1 e2)
+
+        Ann e t ->
+            resultAndThen2
+                (\( ee, et ) ( tt, _ ) ->
+                    if et == tt then
+                        Ok ( ee, tt )
+
+                    else
+                        Err (TypeMismatch et tt)
+                )
+                (eval e)
+                (eval t)
+
+
+resultAndThen2 : (a -> b -> Result err c) -> Result err a -> Result err b -> Result err c
+resultAndThen2 f r1 r2 =
+    Result.andThen (\x -> Result.andThen (f x) r2) r1
