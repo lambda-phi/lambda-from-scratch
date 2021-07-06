0073d4a83b582c370800b6420b887ec18f16a817
David Cavazos <dcavazos@google.com>
2021-07-05 18:10:20 -0700

    use letV

--- src/LambdaFromScratch/UntypedLambdaCalculus.elm
+++ src/LambdaFromScratch/UntypedLambdaCalculus.elm
@@ -7,6 +7,11 @@ type Expr
     | App Expr Expr --      e1 e2   Application
 
 
+letV : String -> Expr -> Expr -> Expr
+letV x ex e =
+    App (Lam x e) ex
+
+
 {-|
 
     -- x  ⇒  x
@@ -19,77 +24,62 @@ type Expr
     eval (App (Var "e1") (Var "e2")) --> App (Var "e1") (Var "e2")
 
     -- x=ex; x  ⇒  ex
-    eval (App (Lam "x" (Var "x")) (Var "ex")) --> Var "ex"
+    eval (letV "x" (Var "ex") (Var "x")) --> Var "ex"
 
     -- x=ex; e  ⇒  e
-    eval (App (Lam "x" (Var "e")) (Var "ex")) --> Var "e"
+    eval (letV "x" (Var "ex") (Var "e")) --> Var "e"
 
     -- x=ex; λy. x  ⇒  λy. ex
-    eval (App (Lam "x" (Lam "y" (Var "x"))) (Var "ex")) --> Lam "y" (Var "ex")
+    eval (letV "x" (Var "ex") (Lam "y" (Var "x"))) --> Lam "y" (Var "ex")
 
     -- x=ex; e1 x  ⇒  e1 ex
-    eval (App (Lam "x" (App (Var "e1") (Var "x"))) (Var "ex")) --> App (Var "e1") (Var "ex")
+    eval (letV "x" (Var "ex") (App (Var "e1") (Var "x"))) --> App (Var "e1") (Var "ex")
 
     -- x=ex; x e2  ⇒  ex e2
-    eval (App (Lam "x" (App (Var "x") (Var "e2"))) (Var "ex")) --> App (Var "ex") (Var "e2")
+    eval (letV "x" (Var "ex") (App (Var "x") (Var "e2"))) --> App (Var "ex") (Var "e2")
 
     -- x=ex; y=ey; x  ⇒  ex
-    eval (App (Lam "x" (App (Lam "y" (Var "x")) (Var "ey"))) (Var "ex")) -- Var "ex"
+    eval (letV "x" (Var "ex") (letV "y" (Var "ey") (Var "x"))) -- Var "ex"
 
     -- x=ex; y=ey; y  ⇒  ey
-    eval (App (Lam "x" (App (Lam "y" (Var "y")) (Var "ey"))) (Var "ex")) -- Var "ey"
+    eval (letV "x" (Var "ex") (letV "y" (Var "ey") (Var "y"))) -- Var "ey"
 
     -- x=ex; y=x; y  ⇒  ex
-    eval (App (Lam "x" (App (Lam "y" (Var "x")) (Var "ey"))) (Var "ex")) -- Var "ex"
+    eval (letV "x" (Var "ex") (letV "y" (Var "x") (Var "y"))) -- Var "ex"
 
 -}
 eval : Expr -> Expr
 eval expr =
     case expr of
-        -- x
-        -- ⇒  x
+        -- x  ⇒  x
         Var x ->
             Var x
 
-        -- λx. e
-        -- ⇒  λx. e
+        -- λx. e  ⇒  λx. e
         Lam x e ->
             Lam x (eval e)
 
-        -- x=ex; λy. e
-        -- ⇒  (λx. λy. e) ex
-        -- ⇒  λy. (x=ex; e)
-        -- ⇒  λy. ((λx. e) ex)
+        -- x=ex; λy. e  ⇒  λy. (x=ex; e)
         App (Lam x (Lam y e)) ex ->
-            eval (Lam y (App (Lam x e) ex))
+            eval (Lam y (letV x ex e))
 
-        -- x=ex; y=ey; e
-        -- ⇒  (λx. (λy. e) ey) ex
-        -- ⇒  y=(x=ex; ey); (x=ex; e)
-        -- ⇒  y=((λx. ey) ex); ((λx. e) ex)
-        -- ⇒  (λy. ((λx. e) ex)) ((λx. ey) ex)
+        -- x=ex; y=ey; e  ⇒  y=(x=ex; ey); (x=ex; e)
         App (Lam x (App (Lam y e) ey)) ex ->
-            eval (App (Lam y (App (Lam x e) ex)) (App (Lam x ey) ex))
+            eval (letV y (letV x ex ey) (letV x ex e))
 
-        -- x=ex; e1 e2
-        -- ⇒  (λx. e1 e2) ex
-        -- ⇒  (x=ex; e1) (x=ex; e2)
-        -- ⇒  ((λx. e1) ex) ((λx. e2) ex)
+        -- x=ex; e1 e2  ⇒  (x=ex; e1) (x=ex; e2)
         App (Lam x (App e1 e2)) ex ->
-            eval (App (App (Lam x e1) ex) (App (Lam x e2) ex))
+            eval (App (letV x ex e1) (letV x ex e2))
 
         App (Lam x e) ex ->
             if e == Var x then
-                -- x=ex; x
-                -- ⇒  ex
+                -- x=ex; x  ⇒  ex
                 eval ex
 
             else
-                -- x=ex; e
-                -- ⇒  e
+                -- x=ex; e  ⇒  e
                 eval e
 
-        -- e1 e2
-        -- ⇒  e1 e2
+        -- e1 e2  ⇒  e1 e2
         App e1 e2 ->
             App (eval e1) (eval e2)
