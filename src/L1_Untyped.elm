521df1a13cc73cc682d39f623da05f9dce0749a9
David Cavazos <dcavazos@google.com>
2021-09-07 14:46:21 -0700

    experiment with fixpoint

--- src/L1_Untyped.elm
+++ src/L1_Untyped.elm
@@ -8,11 +8,11 @@ type alias Env =
 
 
 type Expr
-    = Num Float --              3.14        Number value
+    = Num Float --              3.14        Number constant
     | Var String --             x           Variable
     | App Expr Expr --          e1 e2       Application
     | Lam String Expr --        λx. e       Lambda abstraction
-    | Env Env Expr --           Γ e         Environment
+    | Fix String Expr --        @f. e       Fixed point
 
 
 type Error
@@ -69,9 +69,11 @@ letVar x ex e =
 
 {-|
 
+    --== Number constant ==--
     -- ✅ 3.14  ⇒  3.14
     eval (Num 3.14) empty --> Ok (Num 3.14)
 
+    --== Variable ==--
     -- ❌ x  ⇒  Undefined variable
     eval (Var "x") empty --> Err (UndefinedVar "x")
 
@@ -84,14 +86,30 @@ letVar x ex e =
     -- ✅ x ~ {x=y, y=1}  ⇒  1
     eval (Var "x") (fromList [ ( "x", Var "y" ), ( "y", Num 1 ) ]) --> Ok (Num 1)
 
-    -- ✅ x ~ {x=x}  ⇒  {x=x} x
-    eval (Var "x") (fromList [ ( "x", Var "x" ) ]) --> Ok (Env (fromList [("x", Var "x")]) (Var "x"))
+    -- ✅ x ~ {x=x}  ⇒  x
+    eval (Var "x") (fromList [ ( "x", Var "x" ) ]) --> Ok (Var "x")
 
+    -- ✅ x ~ {x=@x. x}  ⇒  @x. x
+    eval (Var "x") (fromList [ ( "x", Fix "x" (Var "x") ) ]) --> Ok (Fix "x" (Var "x"))
+
+    -- ✅ x ~ {x=@y. y}  ⇒  @y. y
+    eval (Var "x") (fromList [ ( "x", Fix "y" (Var "y") ) ]) --> Ok (Fix "y" (Var "y"))
+
+    --== Application ==--
     -- ❌ 1 2  ⇒  Not a function
     eval (App (Num 1) (Num 2)) empty --> Err (NotAFunction (Num 1))
 
-    -- ❌ x 1  ⇒  Undefined variable
-    eval (App (Var "x") (Num 1)) empty --> Err (UndefinedVar "x")
+    -- ❌ f 1  ⇒  Undefined variable
+    eval (App (Var "f") (Num 1)) empty --> Err (UndefinedVar "f")
+
+    -- ✅ f 1 ~ {f=f}  ⇒  f 1
+    eval (App (Var "f") (Num 1)) (fromList [ ( "f", Var "f" ) ]) --> Ok (App (Var "f") (Num 1))
+
+    -- ✅ f 1 ~ {f=λx. x}  ⇒  1
+    eval (App (Var "f") (Num 1)) (fromList [ ( "f", Lam "x" (Var "x") ) ]) --> Ok (Num 1)
+
+    -- ✅ f 1 ~ {f=@f. f}  ⇒  @f. f 1
+    eval (App (Var "f") (Num 1)) (fromList [ ( "f", Fix "f" (Var "f") ) ]) --> Ok (Fix "f" (App (Var "f") (Num 1)))
 
     -- ✅ (λx. x) 1  ⇒  1
     eval (App (Lam "x" (Var "x")) (Num 1)) empty --> Ok (Num 1)
@@ -105,32 +123,24 @@ letVar x ex e =
     -- ✅ (λx. x) 2 ~ {x=1}  ⇒  2
     eval (App (Lam "x" (Var "x")) (Num 2)) (fromList [ ( "x", Num 1 ) ]) --> Ok (Num 2)
 
-    -- ✅ ({f=f} f) 1  ⇒  {f=f} (f 1)
-    eval (App (Env (fromList [ ( "f", Var "f" ) ]) (Var "f")) (Num 1)) empty --> Ok (Env (fromList [("f", Var "f")]) (App (Var "f") (Num 1)))
+    -- ✅ (@f. f) 1  ⇒  @f. f 1
+    eval (App (Fix "f" (Var "f")) (Num 1)) empty --> Ok (Fix "f" (App (Var "f") (Num 1)))
 
-    -- ✅ ({f=f} f) 1 ~ {x=2}  ⇒  {f=f} (f 1)
-    eval (App (Env (fromList [ ( "f", Var "f" ) ]) (Var "f")) (Num 1)) (fromList [ ( "x", Num 1 ) ]) --> Ok (Env (fromList [("f", Var "f")]) (App (Var "f") (Num 1)))
+    -- ✅ (@f. f) (@x. x)  ⇒  @f. f (@x. x)
+    eval (App (Fix "f" (Var "f")) (Fix "x" (Var "x"))) empty --> Ok (Fix "f" (App (Var "f") (Fix "x" (Var "x"))))
 
-    -- ✅ ({f=f} f) ({x=x} x)  ⇒  {f=f, x=x} (f x)
-    eval (App (Env (fromList [ ( "f", Var "f" ) ]) (Var "f")) (Env (fromList [ ( "x", Var "x" ) ]) (Var "x"))) empty --> Ok (Env (fromList [("f", Var "f"), ("x", Var "x")]) (App (Var "f") (Var "x")))
+    -- ✅ (@f. f) (@x. x)  ⇒  @f. f (@x. x)
+    eval (App (Fix "f" (Var "f")) (Fix "x" (Var "x"))) empty --> Ok (Fix "f" (App (Var "f") (Fix "x" (Var "x"))))
 
-    -- ✅ ({f=f} f) ({x=x} x) ~ {y=1}  ⇒  {f=f, x=x} (f x)
-    eval (App (Env (fromList [ ( "f", Var "f" ) ]) (Var "f")) (Env (fromList [ ( "x", Var "x" ) ]) (Var "x"))) (fromList [ ( "y", Num 1 ) ]) --> Ok (Env (fromList [("f", Var "f"), ("x", Var "x")]) (App (Var "f") (Var "x")))
-
+    --== Lambda abstraction ==--
     -- ✅ λx. 1  ⇒  λx. 1
     eval (Lam "x" (Num 1)) empty --> Ok (Lam "x" (Num 1))
-
-    -- ✅ λx. {x=x} x  ⇒  λx. x
-    eval (Lam "x" (Env (fromList [ ( "x", Var "x" ) ]) (Var "x"))) empty --> Ok (Lam "x" (Var "x"))
 
     -- ✅ λx. x  ⇒  λx. x
     eval (Lam "x" (Var "x")) empty --> Ok (Lam "x" (Var "x"))
 
     -- ❌ λx. y  ⇒  Undefined variable
     eval (Lam "x" (Var "y")) empty --> Err (UndefinedVar "y")
-
-    -- ✅ {y=1} (λx. y)  ⇒  λx. 1
-    eval (Env (fromList [ ( "y", Num 1 ) ]) (Lam "x" (Var "y"))) empty --> Ok (Lam "x" (Num 1))
 
     -- ✅ λx. λy. x  ⇒  λx. λy. x
     eval (Lam "x" (Lam "y" (Var "x"))) empty --> Ok (Lam "x" (Lam "y" (Var "x")))
@@ -141,14 +151,44 @@ letVar x ex e =
     -- ❌ λx. λy. z  ⇒  λx. λy. z
     eval (Lam "x" (Lam "y" (Var "z"))) empty --> Err (UndefinedVar "z")
 
-    -- ✅ [x=y] x ~ [y=1]  ⇒  1
-    eval (Env (fromList [ ( "x", Var "y" ) ]) (Var "x")) (fromList [ ( "y", Num 1 ) ]) --> Ok (Num 1)
+    -- ✅ λx. @f. f x  ⇒  @f. λx. f x
+    eval (Lam "x" (Fix "f" (App (Var "f") (Var "x")))) empty --> Ok (Fix "f" (Lam "x" (App (Var "f") (Var "x"))))
 
-    -- ✅ [x=1] x ~ [x=2]  ⇒  1
-    eval (Env (fromList [ ( "x", Num 1 ) ]) (Var "x")) (fromList [ ( "x", Num 2 ) ]) --> Ok (Num 1)
+    --== Fixed point ==--
+    -- ✅ @x. x  ⇒  @x. x
+    eval (Fix "x" (Var "x")) empty --> Ok (Fix "x" (Var "x"))
 
-    -- ✅ [x=y] ([y=1] x)  ⇒  1
-    eval (Env (fromList [ ( "x", Var "y" ) ]) (Env (fromList [ ( "y", Num 1 ) ]) (Var "x"))) empty --> Ok (Num 1)
+    -- ✅ @x. x ~ {x=1}  ⇒  @x. x
+    eval (Fix "x" (Var "x")) (fromList [ ( "x", Num 1 ) ]) --> Ok (Fix "x" (Var "x"))
+
+    -- ✅ @x. y ~ {y=1}  ⇒  1
+    eval (Fix "x" (Var "y")) (fromList [ ( "y", Num 1 ) ]) --> Ok (Num 1)
+
+    -- ✅ @x. @y. @z. x  ⇒  @x. x
+    eval (Fix "x" (Fix "y" (Fix "z" (Var "x")))) empty --> Ok (Fix "x" (Var "x"))
+
+    -- ✅ @x. @y. @z. z  ⇒  @z. z
+    eval (Fix "x" (Fix "y" (Fix "z" (Var "z")))) empty --> Ok (Fix "z" (Var "z"))
+
+    -- ✅ (λf. f) (λx. f x)  ⇒  @f. λx. f x
+    eval (App (Lam "f" (Var "f")) (Lam "x" (App (Var "f") (Var "x")))) empty --> Ok (Fix "f" (Lam "x" (App (Var "f") (Var "x"))))
+
+    --== Built-in functions ==--
+    -- ✅ (+) 1  ⇒  λb. $add 1 b
+    eval (App (Var "+") (Num 1)) empty --> Ok (Lam "b" (Add (Num 1) (Var "b")))
+
+    -- ✅ (+) 1 2  ⇒  3
+    eval (App (App (Var "+") (Num 1)) (Num 2)) empty --> Ok (Num 3)
+
+
+
+    --== Recursive functions ==--
+    -- f = \n. (n == 0) 1 (n * f (n - 1))
+
+    --== Mutually recursive functions ==--
+    -- https://en.wikipedia.org/wiki/Hofstadter_sequence#Hofstadter_Female_and_Male_sequences
+    -- f = \n. (n == 0) 1 (n - m (f (n - 1)))
+    -- m = \n. (n == 0) 0 (n - f (m (n - 1)))
 
 -}
 eval : Expr -> Env -> Result Error Expr
@@ -162,12 +202,12 @@ eval expr env =
             case get x env of
                 Just ex ->
                     if ex == Var x then
-                        -- x ~ (x=x :: Γ)  ⇒  [x=x] x
-                        Ok (Env (fromList [ ( x, Var x ) ]) (Var x))
+                        -- TODO: rule
+                        Ok (Var x)
 
                     else
-                        -- x ~ (x=ex :: Γ) x  ⇒  ex ~ (x=ex :: Γ)
-                        eval ex env
+                        -- TODO: rule
+                        eval ex (insert x (Fix x (Var x)) env)
 
                 Nothing ->
                     -- x ~ Γ  ⇒  Undefined variable
@@ -177,33 +217,51 @@ eval expr env =
             -- K e2 ~ Γ  ⇒  Not a function
             Err (NotAFunction (Num k))
 
+        App (Var x) e2 ->
+            case get x env of
+                Just e1 ->
+                    if e1 == Var x then
+                        -- TODO: rule
+                        Result.map (App (Var x))
+                            (eval e2 env)
+
+                    else
+                        -- TODO: rule
+                        eval (App e1 e2) env
+
+                Nothing ->
+                    Err (UndefinedVar x)
+
+        App (App a b) e2 ->
+            -- (a b) e2 ~ Γ  ⇒  (a b ~ Γ) e2 ~ {}
+            Result.andThen (\e1 -> eval (App e1 e2) env)
+                (eval (App a b) env)
+
         App (Lam x e) ex ->
             -- (λx. e) ex ~ Γ  ⇒  e ~ (x=ex :: Γ)
             eval e (insert x ex env)
 
-        App (Env vars1 e1) (Env vars2 e2) ->
-            -- (Γ1 e1) (Γ2 e2) ~ Γ  ⇒  (Γ1 ++ Γ2) (e1 e2)
-            Ok (Env (merge vars1 vars2) (App e1 e2))
+        App (Fix x e1) e2 ->
+            case eval (Fix x e1) env of
+                Ok (Fix y e) ->
+                    -- TODO: rule
+                    Ok (Fix y (App e e2))
 
-        App (Env vars e1) e2 ->
-            -- (Γ1 e1) e2 ~ Γ  ⇒  Γ1 (e1 e2)
-            Ok (Env vars (App e1 e2))
+                Ok e ->
+                    eval (App e e2) env
 
-        App e1 e2 ->
-            -- e1 e2 ~ Γ  ⇒  (e1 ~ Γ) (e2 ~ Γ) ~ []
-            Result.map2 App (eval e1 env) (eval e2 env)
-                |> Result.andThen (\e -> eval e empty)
+                Err err ->
+                    Err err
 
         Lam x e ->
             case eval e (insert x (Var x) env) of
-                Ok (Env vars ee) ->
-                    if vars == Dict.singleton x (Var x) then
-                        -- λx. ({x=x} e ~ (x=x :: Γ))  ⇒  λx. e
+                Ok (Fix y ee) ->
+                    if x == y then
                         Ok (Lam x ee)
 
                     else
-                        -- λx. ({x=ex :: Γ1} e ~ (x=x :: Γ2))  ⇒  (Γ1 - x) (λx. e)
-                        Ok (Env (remove x vars) (Lam x ee))
+                        -- TODO: rule
+                        Ok (Fix y (Lam x ee))
 
                 Ok ee ->
                     -- λx. (e ~ (x=x :: Γ))  ⇒  λx. e
@@ -212,6 +270,15 @@ eval expr env =
                 Err err ->
                     Err err
 
-        Env vars e ->
-            -- Γ e  ⇒  e ~ Γ
-            eval e (merge vars env)
+        Fix x (Var y) ->
+            if x == y then
+                -- TODO: rule
+                Ok (Fix x (Var x))
+
+            else
+                -- TODO: rule
+                eval (Var y) env
+
+        Fix x e ->
+            -- TODO: rule
+            eval e (insert x (Fix x (Var x)) env)
